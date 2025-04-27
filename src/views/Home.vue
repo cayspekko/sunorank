3823d1ee94d028e5cd0a6d027c927c6866727b10
Doug Applegate <cayspekko@gmai.com>
2025-04-27 17:52:51 -0600

    cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,11 +36,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import { InformationCircle } from '@vicons/ionicons5'
-
-// Define reactive state
-const test = ref<string>('')
 
 // Component name is automatically inferred from filename
 // No need to register components - they're automatically available in the template
